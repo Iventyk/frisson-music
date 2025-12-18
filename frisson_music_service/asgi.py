3bec1c6221e2dea587bd134d2265ca1e9c0b5b94
ivan <ivanventik@gmail.com>
2025-12-18 17:47:58 +0200

    renamed directories frisson_music -> frisson_music_servise, albums -> frisson_music

--- frisson_music_service/asgi.py
+++ frisson_music_service/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for frisson_music project.
+ASGI config for frisson_music_service project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frisson_music.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frisson_music_service.settings')
 
 application = get_asgi_application()
--- frisson_music_service/urls.py
+++ frisson_music_service/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for frisson_music project.
+URL configuration for frisson_music_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
