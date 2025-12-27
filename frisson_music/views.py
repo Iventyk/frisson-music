3aabdaebfe707a10d31a2915fa34197810580ddf
ivan <ivanventik@gmail.com>
2025-12-27 15:43:51 +0200

    fixed a bug with Pagination on Search page: can't go to previous page

--- frisson_music/views.py
+++ frisson_music/views.py
@@ -37,6 +37,13 @@ class AlbumListView(ListView):
             qs = qs.filter(album_title__icontains=search)
         return qs
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query_params = self.request.GET.copy()
+        query_params.pop('page', None)
+        context['query_params'] = query_params.urlencode()
+        return context
+
 
 class AlbumDetailView(DetailView):
     model = Album
